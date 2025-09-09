feb97281afe1103a96c1cccb77f00fcab618c94f
dhruuujs <dhrubajiban78901@gmail.com>
2025-09-09 15:58:28 +0530

    insert front function implemented

--- SingleLinkList.c
+++ SingleLinkList.c
@@ -8,23 +8,29 @@ struct Node{
 
 
 void insertFront(struct Node** head, int data){
-    
+    struct Node* newNode=malloc(sizeof(struct Node));
+    newNode->data=data;
+    newNode->next=*head;
+    *head=newNode;
 }
 
 
 void printList(struct Node* head){
 
     while(head!=NULL){
-        printf("%d",head->data);
+        printf("%d->",head->data);
         head=head->next;
     }
-    printf("NULL");
+    printf("NULL\n");
 }
 
 int main(){
 struct Node* head=NULL;
 
+insertFront(&head,10);
+insertFront(&head,10);
 
+printList(head);
     return 0;
 }
 
